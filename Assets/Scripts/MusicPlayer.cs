b85ba1e2421aa77f1ea87cc22fff32d52e4e3cba
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2021-02-09 08:37:31 -0500

    Completed Lesson 97

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -1,24 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class MusicPlayer : MonoBehaviour
 {
-    [SerializeField] float loadDelay = 2f;
-
     void Awake()
     {
         DontDestroyOnLoad(gameObject);
-    }
-
-    void Start()
-    {
-        Invoke("LoadFirstScene", loadDelay);
-    }
-
-    void LoadFirstScene()
-    {
-        SceneManager.LoadScene(1);
     }
 }
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField] float loadDelay = 2f;
+
+    void Start()
+    {
+        Invoke("LoadFirstScene", loadDelay);
+    }
+
+    void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
