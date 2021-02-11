7fa24068eb6ae863fef64f53e52baf3b7c246afd
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2021-02-11 07:58:43 -0500

    Completed Lesson 103

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    void Start()
+    {
+        AddNonTriggerBoxCollider();
+    }
+
+    void AddNonTriggerBoxCollider()
+    {
+        Collider nonTriggerBoxCollider = gameObject.AddComponent<BoxCollider>();
+        nonTriggerBoxCollider.isTrigger = false;
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        Destroy(gameObject);
+    }
+}
