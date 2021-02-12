de0a3289e891b793808962838a2e7e809d1ada42
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2021-02-12 08:36:47 -0500

    Completed Lesson 111

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,6 +7,7 @@ public class Enemy : MonoBehaviour
     [SerializeField] GameObject deathFX;
     [SerializeField] Transform parent;
     [SerializeField] int scorePerHit = 12;
+    [SerializeField] int hits = 10;
     ScoreBoard scoreBoard;
 
     void Start()
@@ -23,7 +24,19 @@ public class Enemy : MonoBehaviour
 
     void OnParticleCollision(GameObject other)
     {
+        ProcessHit();
+        if (hits < 1)
+            KillEnemy();
+    }
+
+    void ProcessHit()
+    {
         scoreBoard.ScoreHit(scorePerHit);
+        hits--;
+    }
+
+    void KillEnemy()
+    {
         GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
         fx.transform.parent = parent;
         Destroy(gameObject);
